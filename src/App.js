eb14b42cdcd440c15e2385096e9ffd29615c62e4
Ева <eva@MacBook-Air-Eva.local>
2024-08-10 10:56:49 +0500

    update buttons and token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
-import Header from './components/Header/Header';
+import Header from './components/Header/Header.jsx';
+import Button from './components/Button/Button.jsx';
 
 function App() {
   const {tg, onToggleButton} = useTelegram()
@@ -15,7 +16,7 @@ function App() {
     <div className="App">
       <Header/>
       Welcome!
-      <button onClick={onToggleButton}>Toggle</button>
+      <Button onClick={onToggleButton}>Toggle</Button>
     </div>
     </>
   );
